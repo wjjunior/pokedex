918c1f157575eeaa73ee820bfcac730e1bae0291
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-20 14:04:59 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig: Config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|scss|sass)$": "identity-obj-proxy",
+  },
+  testEnvironment: "jsdom",
+};
+
+export default createJestConfig(customJestConfig);
