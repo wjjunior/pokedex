6a3fc7d71d443fbae12b3aebbc145d4b15acea48
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-21 21:49:50 -0300

    test: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -12,23 +12,27 @@ const mockPostRequest = (): HttpPostParams<typeof body> => ({
   body,
 });
 
-const makeSut = (): AxiosHttpClient => {
+const makeSut = (): AxiosHttpClient<typeof body, { success: boolean }> => {
   return new AxiosHttpClient();
 };
+
 describe("AxiosHttpClient", () => {
   test("Should call axios.post with correct URL and body", async () => {
     const request = mockPostRequest();
-    const response = { data: { success: true } };
+    const axiosResponse = {
+      status: 200,
+      data: { success: true },
+    };
 
-    mockedAxios.post.mockResolvedValueOnce(response);
+    mockedAxios.post.mockResolvedValueOnce(axiosResponse);
 
     const sut = makeSut();
-    const result = await sut.post<typeof body, typeof response.data>({
-      url: request.url,
-      body: request.body,
-    });
+    const result = await sut.post(request);
 
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
-    expect(result).toEqual(response.data);
+    expect(result).toEqual({
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    });
   });
 });
