f3bc3e6502a6ab26df20377b901ae04a5b03ee89
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-22 20:11:16 -0300

    refactor: remove tailwind css and use styled components

--- next.config.ts
+++ next.config.ts
@@ -1,54 +1,8 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.module.rules.push(
-      // Rule for global LESS files
-      {
-        test: /\.less$/,
-        exclude: /\.module\.less$/,
-        use: [
-          "css-loader",
-          {
-            loader: "postcss-loader", // Process Tailwind CSS directives
-          },
-          {
-            loader: "less-loader",
-            options: {
-              lessOptions: {
-                javascriptEnabled: true,
-              },
-            },
-          },
-        ],
-      },
-      // Rule for modular LESS files
-      {
-        test: /\.module\.less$/,
-        use: [
-          "css-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true, // Enable CSS Modules
-            },
-          },
-          {
-            loader: "postcss-loader", // Process Tailwind CSS directives
-          },
-          {
-            loader: "less-loader",
-            options: {
-              lessOptions: {
-                javascriptEnabled: true,
-              },
-            },
-          },
-        ],
-      },
-    );
-
-    return config;
+  compiler: {
+    styledComponents: true,
   },
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,6 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [],
 } satisfies Config;
