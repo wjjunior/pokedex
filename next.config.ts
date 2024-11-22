30df0d1d2fd05e3c204e84325114aa54502e8377
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-21 23:30:10 -0300

    chore: add less

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,48 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.module.rules.push(
+      {
+        test: /\.less$/,
+        exclude: /\.module\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.module\.less$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
