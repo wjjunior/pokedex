67a25778368d8271421710819a468ade28e87c02
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-23 11:56:14 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-pokemon-list/load-pokemon-list.spec.ts
+++ src/data/usecases/load-pokemon-list/load-pokemon-list.spec.ts
@@ -2,11 +2,23 @@ import { HttpGetClientSpy } from "@/data/test";
 import chance from "chance";
 import { LoadPokemonList } from "./load-pokemon-list";
 
+type SutTypes = {
+  sut: LoadPokemonList;
+  httpGetClientSpy: HttpGetClientSpy;
+};
+const makeSut = (url = chance().url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy();
+  const sut = new LoadPokemonList(url, httpGetClientSpy);
+  return {
+    sut,
+    httpGetClientSpy,
+  };
+};
+
 describe("RemoteLoadSurveyList", () => {
   test("Should call HttpGetClient with correct URL", async () => {
     const url = chance().url();
-    const httpGetClientSpy = new HttpGetClientSpy();
-    const sut = new LoadPokemonList(url, httpGetClientSpy);
+    const { sut, httpGetClientSpy } = makeSut(url);
     await sut.load();
     expect(httpGetClientSpy.url).toBe(url);
   });
