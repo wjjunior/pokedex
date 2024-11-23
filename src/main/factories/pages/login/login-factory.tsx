3d3f6e1acebfb41d329942c992bccc708f5806b5
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-22 23:50:55 -0300

    feat: use makeLogin on page component

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { Login } from "@/presentation/pages";
 import { makeRemoteAuthentication } from "@/main/factories/usecases/authentication/remote-authentication-factory";
 
-export const makeLogin: React.FC = () => {
+export const makeLogin = () => {
   return <Login authentication={makeRemoteAuthentication()} />;
 };
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Login } from "@/presentation/pages";
+import { makeLogin } from "@/main/factories/pages/login/login-factory";
 import React from "react";
 
 export default function Login() {
+  const LoginComponent = makeLogin();
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <Login />
+      {LoginComponent}
     </div>
   );
 }
