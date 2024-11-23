d936af4024cdb74f5e9bd56b05c4dd1e47a3938c
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-22 22:55:41 -0300

    fix: change AxiosHttpClient to handle exceptions

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -7,10 +7,13 @@ import axios, { AxiosResponse } from "axios";
 
 export class AxiosHttpClient<T, R> implements HttpPostClient<T, R> {
   async post(params: HttpPostParams<T>): Promise<HttpResponse<R>> {
-    const httpResponse: AxiosResponse<R> = await axios.post(
-      params.url,
-      params.body,
-    );
+    let httpResponse: AxiosResponse<R>;
+    try {
+      httpResponse = await axios.post(params.url, params.body);
+    } catch (error) {
+      const axiosError = error as { response: AxiosResponse<R> };
+      httpResponse = axiosError.response;
+    }
     return {
       statusCode: httpResponse.status,
       body: httpResponse.data,
--- src/infra/test/mock-axios.ts
+++ src/infra/test/mock-axios.ts
@@ -3,14 +3,16 @@ import chance from "chance";
 
 const Chance = new chance.Chance();
 
+export const mockHttpResponse = () => ({
+  data: {
+    [Chance.string({ length: 5 })]: Chance.string(),
+    [Chance.string({ length: 5 })]: Chance.integer(),
+  },
+  status: Chance.integer({ min: 200, max: 599 }),
+});
+
 export const mockAxios = (): jest.Mocked<typeof axios> => {
   const mockedAxios = axios as jest.Mocked<typeof axios>;
-  mockedAxios.post.mockResolvedValue({
-    data: {
-      [Chance.string({ length: 5 })]: Chance.string(),
-      [Chance.string({ length: 5 })]: Chance.integer(),
-    },
-    status: Chance.integer({ min: 200, max: 599 }),
-  });
+  mockedAxios.post.mockResolvedValue(mockHttpResponse());
   return mockedAxios;
 };
