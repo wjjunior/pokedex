2aa333443e879cca7f76381471b96e0953b7e4f0
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-23 12:11:52 -0300

    test: ensure LoadPokemonList throws on 404

--- src/data/usecases/load-pokemon-list/load-pokemon-list.spec.ts
+++ src/data/usecases/load-pokemon-list/load-pokemon-list.spec.ts
@@ -1,6 +1,8 @@
 import { HttpGetClientSpy } from "@/data/test";
 import chance from "chance";
 import { LoadPokemonList } from "./load-pokemon-list";
+import { HttpStatusCode } from "@/data/protocols/http";
+import { UnexpectedError } from "@/domain/errors";
 
 type SutTypes = {
   sut: LoadPokemonList;
@@ -21,5 +23,23 @@ describe("LoadPokemonList", () => {
     const { sut, httpGetClientSpy } = makeSut(url);
     await sut.load();
     expect(httpGetClientSpy.url).toBe(url);
+  });
+
+  test("Should throw UnexpectedError if HttpGetClient returns 403", async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden,
+    };
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test("Should throw UnexpectedError if HttpGetClient returns 404", async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.notFound,
+    };
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
