240468d22dd196ffc551690d7ba682067ce0cd62
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-23 12:13:14 -0300

    test: ensure LoadPokemonList throws on 500

--- src/data/usecases/load-pokemon-list/load-pokemon-list.spec.ts
+++ src/data/usecases/load-pokemon-list/load-pokemon-list.spec.ts
@@ -42,4 +42,13 @@ describe("LoadPokemonList", () => {
     const promise = sut.load();
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
+
+  test("Should throw UnexpectedError if HttpGetClient returns 500", async () => {
+    const { sut, httpGetClientSpy } = makeSut();
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+    const promise = sut.load();
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
 });
