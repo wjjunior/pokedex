4f7002648dccc15db8a2af7f39f8565983d06bf7
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2024-11-22 21:22:17 -0300

    test: ensure Login calls Authentication with correct values

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Login from "./login";
 import { theme } from "@/presentation/theme/theme";
 import { ThemeProvider } from "styled-components";
+import { AccountModel } from "@/domain/models";
+import { mockAccountModel } from "@/domain/test";
+import chance from "chance";
+
+import {
+  Authentication,
+  AuthenticationParams,
+} from "@/domain/usecases/authentication";
+
+class AuthenticationSpy implements Authentication {
+  account = mockAccountModel();
+  params?: AuthenticationParams;
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
+    this.params = params;
+    return Promise.resolve(this.account);
+  }
+}
 
 describe("Login Component", () => {
   test("Should start with initial state", () => {
@@ -12,13 +30,37 @@ describe("Login Component", () => {
     }) as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
   });
+
+  test("Should call Authentication with correct values", async () => {
+    const { getByPlaceholderText, getByRole, authenticationSpy } =
+      getRenderer();
+
+    const emailInput = getByPlaceholderText("Email");
+    const email = chance().email();
+    await userEvent.type(emailInput, email);
+    const passwordInput = getByPlaceholderText("Password");
+    const password = chance().string({
+      length: 12,
+    });
+    await userEvent.type(passwordInput, password);
+
+    const submitButton = getByRole("button", {
+      name: "Login",
+    }) as HTMLButtonElement;
+    await userEvent.click(submitButton);
+    expect(authenticationSpy.params).toEqual({
+      email,
+      password,
+    });
+  });
 });
 
 function getRenderer() {
+  const authenticationSpy = new AuthenticationSpy();
   const renderer = render(
     <ThemeProvider theme={theme}>
-      <Login />
+      <Login authentication={authenticationSpy} />
     </ThemeProvider>,
   );
-  return { ...renderer };
+  return { ...renderer, authenticationSpy };
 }
